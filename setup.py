3bd3712505daf204060727968c8c4cdaca4a9363
FoteinosMerg <foteinosmerg@gmail.com>
2018-12-09 13:18:06 +0100

    packaging preps

--- setup.py
+++ setup.py
@@ -14,18 +14,21 @@ setup(
     version=pymerkle.__version__,
     author="Foteinos Mergoupis",
     author_email="foteinosmerg@gmail.com",
-    description="A Python library for constructing merkle-trees and performing Log Proofs",
+    description="A Python library for constructing Merkle Trees and validating Log Proofs",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="http://github.com/FoteinosMerg/pymerkle",
     packages=find_packages(),
     classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Security",
         "Topic :: Security :: Cryptography",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     license="License :: OSI Approved :: MIT License",
-    keywords="security encryption")
+    keywords="merkle audit consistency proof log security encryption")
