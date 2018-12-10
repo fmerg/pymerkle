340df4aba26c62de4aba31943e91cb67ccc387b7
FoteinosMerg <foteinosmerg@gmail.com>
2018-12-10 16:13:06 +0100

    remove python3

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
