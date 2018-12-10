d18bd519e2396419718ad3aa692bbeecba84df0c
FoteinosMerg <foteinosmerg@gmail.com>
2018-12-10 15:14:41 +0100

    setup complete

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Security :: Cryptography",
