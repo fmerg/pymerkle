8a9dcf03017665f2a4816c9260a9b2d2a02326c0
Foteinos <foteinosmerg@gmail.com>
2019-01-23 18:58:53 +0100

    change version

--- pymerkle/__init__.py
+++ pymerkle/__init__.py
@@ -3,7 +3,7 @@ from .validations import validate_proof, proof_validator
 
 name = "pymerkle"  # Just for verifying correct installation
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = (
     'merkle_tree',
     'validate_proof',
