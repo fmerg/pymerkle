ae803126ef3c48f2fb26e93aea18cbdfcad03372
Foteinos <foteinosmerg@gmail.com>
2019-01-30 20:17:32 +0100

    modify docs

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,2 +1,2 @@
-pymerkle
-========
+pymerkle Documentation
+======================
--- pymerkle/__init__.py
+++ pymerkle/__init__.py
@@ -1,7 +1,7 @@
 from .tree import merkle_tree
 from .validations import validate_proof, proof_validator
 
-name = "pymerkle"  # Just for verifying correct installation
+name = "pymerkle"
 
 __version__ = "0.1.3"
 __all__ = (
