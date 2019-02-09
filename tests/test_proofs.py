4f73f2d909b496c8247994731c5cbfbe482c8bd8
Foteinos <foteinosmerg@gmail.com>
2019-02-09 20:41:03 +0100

    improve proof tests

--- tests/test_proofs.py
+++ tests/test_proofs.py
@@ -4,11 +4,13 @@ from pymerkle.proof import proof
 import json
 
 # Generate proofs from a one-thousand leaves Merkle-tree
-tree = merkle_tree(*(bytes('{i}-th record', 'utf-8') for i in range(0, 1000)))
-p = tree.audit_proof(666)  # Genuine proof
-q = tree.audit_proof(1000)  # Empty proof
+tree = merkle_tree(*(bytes('{}-th record'.format(i), 'utf-8')
+                     for i in range(0, 1000)))
+p = tree.audit_proof(666)  # Genuine index-based proof
+q = tree.audit_proof(1000)  # Empty index-based proof
 
-# Construct replicates in all possible ways
+# ------------------- Check replicates in all possible ways -------------------
+
 p_1 = proof(from_json=p.JSONstring())
 p_2 = proof(from_dict=json.loads(p.JSONstring()))
 q_1 = proof(from_json=q.JSONstring())
@@ -28,3 +30,18 @@ def test_p_replicates_via_validation(replicate):
 @pytest.mark.parametrize('replicate', (q_1, q_2))
 def test_q_replicates_via_serialization(replicate):
     assert q.serialize() == replicate.serialize()
+
+# ----------------------- Check record based generations -----------------
+
+
+p_3 = tree.audit_proof('665-th record')
+p_4 = tree.audit_proof(b'665-th record')
+
+
+@pytest.mark.parametrize('r', (p_3, p_4))
+def test_record_based_audit_proof(r):
+    assert tree.multi_hash(
+        p.body['proof_path'],
+        p.body['proof_index']) == tree.multi_hash(
+        r.body['proof_path'],
+        p.body['proof_index'])
