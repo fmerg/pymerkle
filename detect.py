9feca4f7a141d6e4b737f7c964bb930ed24ef713
Foteinos <foteinosmerg@gmail.com>
2019-03-30 10:01:43 +0100

    update dev tools

--- detect.py
+++ detect.py
@@ -3,35 +3,36 @@ import time
 
 a = MerkleTree()
 b = MerkleTree()
-print('b', 'a')
+print('b >= a')
 
 t = b.inclusionTest(old_hash=a.rootHash(), sublength=a.length())
+v = validateProof(target_hash=b.rootHash(),proof=b.consistencyProof(
+    old_hash=a.rootHash(),sublength=a.length()))
 time.sleep(1)
-print(0, 0, t)
+print(t, v, 0, 0)
 
 
 b.update(str(1))
 t = b.inclusionTest(old_hash=a.rootHash(), sublength=a.length())
+v = validateProof(target_hash=b.rootHash(),proof=b.consistencyProof(old_hash=a.rootHash(),sublength=a.length()))
 time.sleep(1)
-print(1, 0, t)
+print(t, v, 1, 0)
 
 i = 1
 power = 0
 while i > 0:
     b.update(str(i + 1))
     a.update(str(i))
-    if not not b.inclusionTest(
+    t = b.inclusionTest(
         old_hash=a.rootHash(),
-        sublength=a.length()) or validateProof(
+        sublength=a.length())
+    v = validateProof(
         target_hash=b.rootHash(),
         proof=b.consistencyProof(
             old_hash=a.rootHash(),
-            sublength=a.length())):
+            sublength=a.length()))
+    if not t or not v:
         time.sleep(1)
-        print(i + 1, i, power, 2**power)
+        print(t, v, i + 1, i, power, 2**power)
         power += 1
-    # if not b.inclusionTest(old_hash=a.rootHash(), sublength=a.length()):
-    #     time.sleep(1)
-    #     print(i + 1, i, power, 2**power)
-    #     power += 1
     i += 1
