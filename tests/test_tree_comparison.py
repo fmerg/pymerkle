3e8a097e4095e4a3349e61fc42982d484fcf1d09
FoteinosMerg <you@example.com>
2019-06-29 09:41:18 +0200

    expand tests

--- tests/test_tree_comparison.py
+++ tests/test_tree_comparison.py
@@ -6,27 +6,33 @@ from pymerkle.exceptions import InvalidTypesException, InvalidComparison
 # ----------------------------------- Setuo -----------------------------------
 
 HASH_TYPES = hashing.HASH_TYPES
-ENCODINGS = hashing.ENCODINGS
+ENCODINGS  = hashing.ENCODINGS
 
 # Files to encrypt
 
-short_APACHE_log = os.path.join(os.path.dirname(__file__), 'logs/short_APACHE_log')
+short_APACHE_log  = os.path.join(os.path.dirname(__file__), 'logs/short_APACHE_log')
 RED_HAT_LINUX_log = os.path.join(os.path.dirname(__file__), 'logs/RED_HAT_LINUX_log')
 
 # ~ Generate trees (for all combinations of hash and encoding types)
 # ~ along with valid parameters for inclusion test
 
 trees_and_subtrees = []
+
 for security in (True, False):
     for hash_type in HASH_TYPES:
         for encoding in ENCODINGS:
+
             tree = MerkleTree(
                 hash_type=hash_type,
                 encoding=encoding,
-                security=security)
+                security=security
+            )
+
             tree.encryptFilePerLog(short_APACHE_log)
             old_hash, sublength = tree.rootHash, tree.length
+
             tree.encryptFilePerLog(RED_HAT_LINUX_log)
+
             trees_and_subtrees.append((tree, old_hash, sublength))
 
 
@@ -169,41 +175,48 @@ def test_not___lt___InvalidComparison():
 
 # -------------- Test inclusion for sublength equal to power of 2 --------
 
-# ~ Passing the following tests indicates that the bug concerning inclusion
-# ~ tests (or, implicitly, consistency proofs) for sublengths equal to
-# ~ powers of 2 has been fixed (in particular, the ``multi_hash``
-# ~ functionality has been modified appropriately for the case of
-# ~ one-member-sequences, so that this issue does not arise)
-
+# ~ Passing the following tests indicates that the bug concerning inclusion tests (or,
+# ~ implicitly, consistency proofs) for sublengths equal to powrs of 2 has been fixed
+# ~ (in particular, the ``multi_hash`` function has been modified appropriately for
+# ~ the case of one-member-sequences, so that this issue does not aris
 
 # Initialize parametrization with the one-leaf tree
+
 trees_and_later_states = [(
     MerkleTree('1'), MerkleTree(*[str(k) for k in range(1, j)])
 ) for j in range(2, 10)]
 
 # Parametrize for the first 9 powers of 2 beginning from 2^1
-for power in range(1, 10):
-    tree = MerkleTree(*[str(i) for i in range(2, 2**power + 1)])
+
+for _power in range(1, 10):
+
+    tree = MerkleTree(*[str(i) for i in range(2, 2**_power + 1)])
+
     for j in range(1, 10):
-        trees_and_later_states.append((
-            tree,
-            MerkleTree(*[str(k) for k in range(2, 2**power + 1 + j)])
-        ))
+        trees_and_later_states.append(
+            (
+                tree,
+                MerkleTree(*[str(k) for k in range(2, 2**_power + 1 + j)])
+            )
+        )
 
 
-@pytest.mark.parametrize('tree, later_state', trees_and_later_states)
-def test_inclusion_test_with_sublength_equal_to_power_of_2(tree, later_state):
-    assert later_state.inclusionTest(
-        old_hash=tree.rootHash,
-        sublength=tree.length) is True
+@pytest.mark.parametrize('_tree, _later_state', trees_and_later_states)
+def test_inclusion_test_with_sublength_equal_to_power_of_2(_tree, _later_state):
+
+    assert _later_state.inclusionTest(
+        _tree.rootHash,
+        _tree.length
+    )
 
 
-# @pytest.mark.parametrize('tree, later_state', trees_and_later_states)
-# def test_consistency_proof_validation_with_sublength_equal_to_power_of_2(
-#         tree,
-#         later_state):
-#     assert validateProof(
-#         target_hash=later_state.rootHash,
-#         proof=later_state.consistencyProof(
-#             old_hash=tree.rootHash,
-#             sublength=tree.length)) is True
+@pytest.mark.parametrize('_tree, _later_state', trees_and_later_states)
+def test_consistency_proof_validation_with_sublength_equal_to_power_of_2(_tree, _later_state):
+
+    assert validateProof(
+        _later_state.rootHash,
+        _later_state.consistencyProof(
+            _tree.rootHash,
+            _tree.length
+        )
+    )
