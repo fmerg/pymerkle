ea0db64da6fd644d7e7579bf626116f6f4036d8c
FoteinosMerg <foteinomerg@protonmail.com>
2019-07-10 17:19:12 +0300

    replace serializer with parent class

--- tests/test_serialization_errors.py
+++ tests/test_serialization_errors.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from pymerkle.serializers import MerkleTreeSerializer, LeafSerializer, NodeSerializer, ProofSerializer, ReceiptSerializer
 
 _serializers = [
@@ -14,4 +15,4 @@ class Empty(object): pass
 @pytest.mark.parametrize('_serializer', _serializers)
 def test_serialization_error(_serializer):
     with pytest.raises(TypeError):
-        _serializer.default(_serializer, Empty())
+        json.JSONEncoder.default(_serializer, Empty())
