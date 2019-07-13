a53743bddb832885709470b5191c60aabac9abe0
FoteinosMerg <foteinomerg@protonmail.com>
2019-07-13 23:10:46 +0300

    refine benchmark arguments

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -25,7 +25,7 @@ from pymerkle.validations import validateProof
 
 # ---------------------------------- Helpers ----------------------------------
 
-
+# Size measurement
 
 from numbers import Number
 from collections import Set, Mapping, deque
@@ -72,15 +72,21 @@ def _size(obj_0):
     return inner(obj_0)
 
 
+# Time measurement
+
+from datetime import datetime
+
+_time_elapsed = lambda start: (datetime.now() - start).total_seconds()
+
+
+# Statistics
+
 from decimal import Decimal, ROUND_HALF_UP
 from functools import reduce
 
-def _quantize(_float, precision):
-    return Decimal(_float).quantize(precision, rounding=ROUND_HALF_UP)
+_quantize = lambda _float, precision: Decimal(_float).quantize(precision, rounding=ROUND_HALF_UP)
 
 def _mean_value(_list, precision, with_stdev=False):
-    """Accepts an iterable of numbers, returns a string
-    """
 
     _list = [_quantize(_, precision) for _ in _list]
 
@@ -100,57 +106,28 @@ def _mean_value(_list, precision, with_stdev=False):
         return mean, stdev
 
 
-from datetime import datetime
-
-_time_elapsed = lambda start: (datetime.now() - start).total_seconds()
-
+# Stats display
 
 def _show_stats(message, mean, stdev, total=None, min=None, max=None):
     sys.stdout.write('\n')
     sys.stdout.write('\n%s' % message)
     sys.stdout.write('\n')
-    if total is not None:
-        sys.stdout.write('\nTotal time : %s' % total)
+    sys.stdout.write('\nMean  : %s' % mean)
+    sys.stdout.write('\nStDev : %s' % stdev)
     if min is not None:
-        sys.stdout.write('\nMax time   : %s' % max)
+        sys.stdout.write('\nMax   : %s' % max)
     if max is not None:
-        sys.stdout.write('\nMin time   : %s' % min)
-    sys.stdout.write('\nMean       : %s' % mean)
-    sys.stdout.write('\nStDev      : %s' % stdev)
+        sys.stdout.write('\nMin   : %s' % min)
+    if total is not None:
+        sys.stdout.write('\nTotal : %s' % total)
 
 
 # --------------------------------- Benchmarks ---------------------------------
 
 
-HASH_TYPE    = 'sha256'
-ENCODING     = 'utf-8'
-
-MACHINE      = hash_machine(hash_type=HASH_TYPE, encoding=ENCODING)
-ENCODING     = MACHINE.ENCODING
-HASH         = MACHINE.hash
-
-TREE         = MerkleTree(hash_type=HASH_TYPE, encoding=ENCODING)
-LENGTH       = 10 ** 3 # 4 # 5
-ADDITIONAL   = 10 ** 3 # 4 # 5 # 10 ** 6
-
-ITERATIONS   = 10 ** 3
-ROUNDS       = 20 # 10 ** 5
-
-PROOFS       = []
-
-
-
-PRECISION_1    = 12
-PRECISION_2    = 6
-
-precision_1 = Decimal('.%s1' % ('0' * (PRECISION_1 - 1)))
-precision_2 = Decimal('.%s1' % ('0' * (PRECISION_2 - 1)))
-
-
 import timeit
 
-
-def nodes_benchmark():
+def attribute_access_benchmark():
 
     def access_digest_func(_node):
         def access_attribute(): _node.digest
@@ -172,10 +149,6 @@ def nodes_benchmark():
     right = Leaf(hashfunc=HASH, encoding=ENCODING, record='second record...')
     node  = Node(hashfunc=HASH, encoding=ENCODING, left=left, right=right)
 
-    # Size measurement
-
-    sys.stdout.write('\nSize of leaf with encoding %s and hash type %s (bytes): %d' % (ENCODING, HASH_TYPE, _size(left)))
-    sys.stdout.write('\nSize of internal node with encoding %s and hash type %s (bytes): %d' % (ENCODING, HASH_TYPE, _size(left)))
 
     # Digest access
 
@@ -185,8 +158,11 @@ def nodes_benchmark():
         number=ROUNDS
     )
 
+
     mean, stdev = _mean_value(access_digest_measurements, precision=precision_1, with_stdev=True)
+
     _show_stats('Time needed to access digest (secs)', mean, stdev)
+
 
     # Left parent access
 
@@ -197,7 +173,9 @@ def nodes_benchmark():
     )
 
     mean, stdev = _mean_value(access_left_parent_measurements, precision=precision_1, with_stdev=True)
+
     _show_stats('Time needed to access left parent (secs)', mean, stdev)
+
 
     # Right parent access
 
@@ -208,7 +186,9 @@ def nodes_benchmark():
     )
 
     mean, stdev = _mean_value(access_right_parent_measurements, precision=precision_1, with_stdev=True)
+
     _show_stats('Time needed to access right parent (secs)', mean, stdev)
+
 
     # Child access
 
@@ -219,45 +199,113 @@ def nodes_benchmark():
     )
 
     mean, stdev = _mean_value(access_child_measurements, precision=precision_1, with_stdev=True)
+
     _show_stats('Time needed to access child (secs)', mean, stdev)
 
 
-def tree_benchmark():
+
+def tree_generation_benchmark():
+
     global TREE
 
-    sys.stdout.write('\n')
-    sys.stdout.write('\nTree size with 0 nodes (bytes): %d' % _size(TREE))
-    START = datetime.now()
+    start = datetime.now()
 
     TREE = MerkleTree(*['%d-th record' % _ for _ in range(LENGTH)], hash_type=HASH_TYPE, encoding=ENCODING)
 
-    time_needed = _time_elapsed(START)
+    time_needed = _time_elapsed(start)
 
-    sys.stdout.write('\n')
-    sys.stdout.write('\nTime needed to generate a Merkle-tree with %d leaves (secs): %s' % (LENGTH, time_needed))
-    sys.stdout.write('\nSize of tree with %d leaves (bytes): %d' % (TREE.length, _size(TREE)))
+    sys.stdout.write('\n\nTime needed to generate a Merkle-tree with %d leaves (secs): %s' % (TREE.length, time_needed))
 
 
-    START = datetime.now()
+
+def sizes_benchmark():
+
+
+    # Tree measurement
+
+    sys.stdout.write('\n\nSize of tree with %d leaves (bytes): %d' % (TREE.length, _size(TREE)))
+
+
+    # Leaves measurement
+
+    MAX    = None
+    MIN    = None
+    TOTAL  = 0.0
+
+    _sizes = []
+
+    for _leaf in TREE.leaves:
+
+        _current_size = _size(_leaf)
+
+        if MAX is None:
+            MAX = _current_size
+            MIN = _current_size
+        else:
+            MAX = max(_current_size, MAX)
+            MIN = min(_current_size, MIN)
+
+        TOTAL += _current_size
+        _sizes.append(_current_size)
+
+
+    mean, stdev = _mean_value(_sizes, precision=Decimal('1'), with_stdev=True)
+
+    _show_stats(message='Leaves size', total=TOTAL, min=MIN, max=MAX, mean=mean, stdev=stdev)
+
+
+    # Internal nodes measurement
+
+    MAX    = None
+    MIN    = None
+    TOTAL  = 0.0
+
+    _sizes = []
+
+    for _node in TREE.nodes - set(TREE.leaves):
+
+        _current_size = _size(_node)
+
+        if MAX is None:
+            MAX = _current_size
+            MIN = _current_size
+        else:
+            MAX = max(_current_size, MAX)
+            MIN = min(_current_size, MIN)
+
+        TOTAL += _current_size
+        _sizes.append(_current_size)
+
+
+    mean, stdev = _mean_value(_sizes, precision=Decimal('1'), with_stdev=True)
+
+    _show_stats(message='Internal nodes size', total=TOTAL, min=MIN, max=MAX, mean=mean, stdev=stdev)
+
+
+
+def encryption_benchmark():
+
+    # Massive encryption measurement
+
+    start = datetime.now()
 
     for _ in range(LENGTH, LENGTH + ADDITIONAL):
         TREE.encryptRecord('%d-th record' % _)
 
-    time_needed = _time_elapsed(START)
+    time_needed = _time_elapsed(start)
 
-    sys.stdout.write('\n')
-    sys.stdout.write('\nTime needed to update the tree with %d leaves (secs): %f' % (ADDITIONAL, time_needed))
+    sys.stdout.write('\n\nTime needed to update the tree with %d leaves (secs): %f' % (ADDITIONAL, time_needed))
     sys.stdout.write('\nSize of tree with %d leaves (bytes): %d' % (TREE.length, _size(TREE)))
 
 
-    #
+    # Single encryption measurements
 
-    START = datetime.now()
     MAX   = None
     MIN   = None
     TOTAL = 0.0
 
     elapsed = []
+    start = datetime.now()
 
     for _ in range(TREE.length, TREE.length + ADDITIONAL):
 
@@ -277,7 +325,9 @@ def tree_benchmark():
         TOTAL += _elapsed
         elapsed.append(_elapsed)
 
+
     mean, stdev = _mean_value(elapsed, precision_2, with_stdev=True)
+
     _show_stats(message='Tree update', total=_quantize(TOTAL, precision_2), min=MIN, max=MAX, mean=mean, stdev=stdev)
 
 
@@ -301,9 +351,6 @@ def audit_proofs_benchmark():
 
         _elapsed = _time_elapsed(_cycle)
 
-        # print(_proof)
-        # print(_)
-
         if MAX is None:
             MAX = _elapsed
             MIN = _elapsed
@@ -315,30 +362,31 @@ def audit_proofs_benchmark():
         TOTAL += _elapsed
         elapsed.append(_elapsed)
 
+
     mean, stdev = _mean_value(elapsed, precision_2, with_stdev=True)
+
     _show_stats(message='Audit proofs', total=_quantize(TOTAL, precision_2), min=MIN, max=MAX, mean=mean, stdev=stdev)
+
 
 
 def consistency_proofs_benchmark():
 
     # Generate states
 
-    global TREE
     TREE = MerkleTree('0-th record', encoding=ENCODING, hash_type=HASH_TYPE)
 
-    STATES = []
-    for _ in range(1, LENGTH + 3 * ADDITIONAL):#):
+    states = []
 
-        STATES.append((TREE.rootHash, TREE.length))
+    for _ in range(1, LENGTH + 3 * ADDITIONAL):
+
+        states.append((TREE.rootHash, TREE.length))
         TREE.encryptRecord(bytes('%d-th record' % _, encoding=ENCODING))
 
-    STATES.append((TREE.rootHash, TREE.length))
+    states.append((TREE.rootHash, TREE.length))
 
     # Generate proofs
 
     global PROOFS
-
-    sys.stdout.write('\nConsistency proofs')
 
     START = datetime.now()
     MAX   = None
@@ -349,19 +397,14 @@ def consistency_proofs_benchmark():
 
     for _ in range(TREE.length):
 
-        # print(_)
-
-        _oldhash   = STATES[_][0]
-        _sublength = STATES[_][1]
+        _oldhash   = states[_][0]
+        _sublength = states[_][1]
 
         _cycle   = datetime.now()
 
         _proof = TREE.consistencyProof(oldhash=_oldhash, sublength=_sublength)
 
         _elapsed = _time_elapsed(_cycle)
-
-        # print(_proof)
-        # print(_)
 
         if MAX is None:
             MIN = MAX = _elapsed
@@ -373,14 +416,14 @@ def consistency_proofs_benchmark():
         TOTAL += _elapsed
         elapsed.append(_elapsed)
 
+
     mean, stdev = _mean_value(elapsed, precision_2, with_stdev=True)
+
     _show_stats(message='Consistency proofs' , total=_quantize(TOTAL, precision_2), min=MIN, max=MAX, mean=mean, stdev=stdev)
 
 
 
 def proof_validations_benchmark():
-
-    global PROOFS
 
     START = datetime.now()
     MAX   = None
@@ -408,15 +451,43 @@ def proof_validations_benchmark():
         TOTAL += _elapsed
         elapsed.append(_elapsed)
 
+
     mean, stdev = _mean_value(elapsed, precision_2, with_stdev=True)
+
     _show_stats(message='Proof validations', total=_quantize(TOTAL, precision_2), min=MIN, max=MAX, mean=mean, stdev=stdev)
+
 
 # ------------------------------------ main ------------------------------------
 
+HASH_TYPE    = None
+ENCODING     = None
+HASH         = None
+LENGTH       = None
+ADDITIONAL   = None
+ITERATIONS   = None
+ROUNDS       = None
+
+TREE         = None
+PROOFS       = []
+
+PRECISION_1    = 12
+PRECISION_2    = 6
+
+precision_1 = Decimal('.%s1' % ('0' * (PRECISION_1 - 1)))
+precision_2 = Decimal('.%s1' % ('0' * (PRECISION_2 - 1)))
+
 def main():
 
+    global HASH_TYPE
+    global ENCODING
+    global HASH
+    global LENGTH
+    global ADDITIONAL
+    global ITERATIONS
+    global ROUNDS
+
     prog = sys.argv[0]
-    usage = 'python3 ... %s' % prog
+    usage = 'python3 ... %s [--hashtype] [--encoding] [--length] [--additional] [--iterations] [--rounds]' % prog
 
     parser = argparse.ArgumentParser(
         prog=prog,
@@ -427,48 +498,66 @@ def main():
 
     parser.add_argument(
         '--hashtype',
+        type=str,
         help='Hashing algorithm used by the Merkle-tree',
-        dest='hashtype',
         default='sha256'
     )
 
     parser.add_argument(
-        '-e', '--encoding',
-        help='Encoding used by the Merkle-tree before hashing',
-        dest='encoding',
+        '--encoding',
+        type=str,
+        help='Encoding used by the Merkle-tree',
         default='utf_8'
     )
 
     parser.add_argument(
-        '-s', '--security',
-        help='Security mode of the Merkle-tree',
-        dest='security',
-        default=True
+        '--length',
+        type=int,
+        help='Initial number of leaves',
+        default=1000
     )
 
     parser.add_argument(
-        '-c', '--contentpath',
-        help='Relative path to a file whose content will be encrypted',
-        dest='contentpath',
-        default='../pymerkle/tests/logs/short_APACHE_log'
+        '--additional',
+        type=int,
+        help='Additional number of leaves to append',
+        default=1000
     )
 
     parser.add_argument(
-        '--logfile',
-        help='Relative path to a log-file under encryption',
-        dest='logfile',
-        default='../pymerkle/tests/logs/short_APACHE_log'
+        '--iterations',
+        type=int,
+        help='Number of iterations when accessing attributes',
+        default=1000
     )
 
     parser.add_argument(
-        '-o', '--objectfile',
-        help='Relative path to a .json file',
-        dest='objectfile',
-        default='../pymerkle/tests/objects/sample.json'
+        '--rounds',
+        type=int,
+        help='Number of rounds when accessing attributes',
+        default=20
     )
 
-    nodes_benchmark()
-    tree_benchmark()
+    parsed_args = parser.parse_args()
+
+    HASH_TYPE  = parsed_args.hashtype
+    ENCODING   = parsed_args.encoding
+    _machine   = hash_machine(hash_type=HASH_TYPE, encoding=ENCODING)
+    HASH       = _machine.hash
+    LENGTH     = parsed_args.length
+    ADDITIONAL = parsed_args.additional
+    ITERATIONS = parsed_args.iterations
+    ROUNDS     = parsed_args.rounds
+
+
+    # To do:
+    # Include precision in arguments
+    # Beautify printing
+
+    attribute_access_benchmark()
+    tree_generation_benchmark()
+    sizes_benchmark()
+    encryption_benchmark()
     audit_proofs_benchmark()
     consistency_proofs_benchmark()
     proof_validations_benchmark()
